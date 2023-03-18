a1af86f5460faec25ac05c3dbb8b85d42da3527d
ChrisChiko266 <102423785+ChrisChiko266@users.noreply.github.com>
2023-03-18 13:25:02 +0200

    2

--- properties/urls.py
+++ properties/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('', views.PropertiesHome.as_view(), name='home'),
     path('contact/', views.Contact.as_view(), name='contact'),
     path('about/', views.AboutUs.as_view(), name='about'),
+    path('properties/', views.PropertyListing.as_view(), name='all-property'),
+    path('blog/list/', views.BlogList.as_view(), name='blog-list'),
+    path('blog/grid/', views.BlogGrid.as_view(), name='blog-grid'),
+    path('agency/list/', views.AgencyList.as_view(), name='agency-list'),
+    path('agent/list/', views.AgentList.as_view(), name='agent-list'),
 ]
--- properties/views.py
+++ properties/views.py
@@ -14,3 +14,44 @@ class Contact(generic.DetailView):
 class AboutUs(generic.DetailView):
     def get(self, request):
         return render(request, 'properties/page-about.html')
+
+
+class PropertyListing(generic.ListView):
+    def get(self, request):
+        return render(request, 'properties/page-listing-v1.html')
+
+
+class PropertyDetail(generic.DetailView):
+    def get(self, request):
+        return render(request, 'properties/page-listing-all.html')
+
+
+class BlogList(generic.ListView):
+    def get(self, request):
+        return render(request, 'properties/page-blog-list.html')
+
+
+class BlogGrid(generic.ListView):
+    def get(self, request):
+        return render(request, 'properties/page-blog-grid.html')
+    
+
+class AgencyList(generic.ListView):
+    def get(self, request):
+        return render(request, 'properties/page-agency-list.html')
+    
+
+class AgencyDetails(generic.DetailView):
+    def get(self, request):
+        return render(request, 'properties/page-agency-single.html')
+
+
+class AgentList(generic.ListView):
+    def get(self, request):
+        return render(request, 'properties/page-agent-list.html')
+    
+
+class AgentDetails(generic.DetailView):
+    def get(self, request):
+        return render(request, 'properties/page-agent-single.html')
+
